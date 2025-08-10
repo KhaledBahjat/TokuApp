e7a34a862f16533c92a3a2d229685ca4e99b56f9
KhaledBahjat <khaledbahjat0@gmail.com>
2025-08-10 18:57:11 +0300

    refactor code and added audioplayerspackege

---
+++ languge_app/lib/components/item.dart
@@ -0,0 +1,70 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:languge_app/models/number.dart';
+
+class Item extends StatelessWidget {
+  const Item({
+    super.key,
+    required this.number,
+  });
+
+  final Number number;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      color: Color(0xfff99531),
+      child: Row(
+        children: [
+          Container(
+            color: Color(0xffFFF6DC),
+            child: Image.asset(number.image),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  number.jpName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  number.enName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Spacer(),
+
+          Padding(
+            padding: EdgeInsetsGeometry.only(right: 15),
+            child: IconButton(
+              onPressed: () {
+                final player = AudioPlayer();
+                player.play(
+                  AssetSource('assets/sounds/numbers/number_eight_sound.mp3'),
+                );
+              },
+              icon: Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- languge_app/lib/screens/numbers_page.dart
+++ languge_app/lib/screens/numbers_page.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:languge_app/components/item.dart';
+import 'package:languge_app/models/number.dart';
 
 class NubmersPage extends StatelessWidget {
   const NubmersPage({super.key});
+  final List<Number> numbers = const [
+    Number(
+      jpName: 'ichi',
+      enName: 'one',
+      image: 'assets/images/numbers/number_one.png',
+    ),
+    Number(
+      jpName: 'Ni',
+      enName: 'two',
+      image: 'assets/images/numbers/number_two.png',
+    ),
+    Number(
+      jpName: 'San',
+      enName: 'three',
+      image: 'assets/images/numbers/number_three.png',
+    ),
+    Number(
+      jpName: 'Shi',
+      enName: 'four',
+      image: 'assets/images/numbers/number_four.png',
+    ),
+    Number(
+      jpName: 'Go',
+      enName: 'five',
+      image: 'assets/images/numbers/number_five.png',
+    ),
+    Number(
+      jpName: 'Roku',
+      enName: 'six',
+      image: 'assets/images/numbers/number_six.png',
+    ),
+    Number(
+      jpName: 'Sebun',
+      enName: 'seven',
+      image: 'assets/images/numbers/number_seven.png',
+    ),
+    Number(
+      jpName: 'hachi',
+      enName: 'eight',
+      image: 'assets/images/numbers/number_eight.png',
+    ),
+    Number(
+      jpName: 'Kyū',
+      enName: 'nine',
+      image: 'assets/images/numbers/number_nine.png',
+    ),
+    Number(
+      jpName: 'Jū',
+      enName: 'ten',
+      image: 'assets/images/numbers/number_ten.png',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Numbers'),
+        backgroundColor: Color(0xff46322B),
+      ),
+      body: ListView.builder(
+        itemCount: numbers.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Item(
+            number: numbers[index],
+          );
+        },
+      ),
+    );
   }
 }
